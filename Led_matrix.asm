48884493b5984af0201b26a8de4476528e0589fa
LWSen <happyliuweisen@163.com>
2017-11-22 17:02:16 +0800

    Add files via upload

---
+++ Led_matrix.asm
@@ -0,0 +1,70 @@
+LIST P=F4520
+#include<P18F4520.INC>
+CONFIG	OSC = XT
+CONFIG	WDT = OFF
+CONFIG	LVP = OFF
+	ORG 0x0
+	goto main
+	cblock 0x100
+		led_head:0x3
+		led_data0:0x8
+		led_data1:0x8
+		column:0x1
+		delay_u:0x1
+		delay_h:0x1
+		delay_l:0x1
+		count:0x1
+	endc
+
+	ORG 0x100
+main:	clrf TRISC   ;PORTC controls led data
+		clrf PORTC
+		clrf TRISD   ;PORTD controls column
+		clrf PORTD
+		lfsr 0,led_data
+		movlb 0x1
+		movlw 0x10
+		movwf count
+init0:	movff count,POSTINC0
+		decf count,f
+		bnz init0
+
+		movlw 0x8    ;initialization
+		movwf column  
+		lfsr 0,led_data1
+led1:	movff column,PORTD
+		movff POSTINC0,PORTC
+		call delay
+		incf column,f
+		movf column,w
+		xorlw 0x10
+		bnz led1
+		lfsr 0,led_data0
+led0:	movff column,PORTD
+		movff POSTINC0,PORTC
+		call delay
+		incf column,f
+		movf column,w
+		xorlw 0x18
+		bnz led0
+		movlw 0x8
+		movwf column
+		lfsr 0,led_data1
+		bra led1
+
+delay:	movlw d'0'
+		movwf delay_h
+delay1:	movlw d'0'
+		movwf delay_l
+delay2:	movlw d'2'
+		movwf delay_u
+delay3:	decf delay_u,f
+		bnz delay3
+		decf delay_l,f
+		bnz delay2
+		decf delay_h,f
+		bnz delay1
+		return
+END
+		
+		
